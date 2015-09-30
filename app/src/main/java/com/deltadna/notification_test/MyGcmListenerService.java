91bf12d90595a07f782bb86ac5397e7e84d80559
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2015-09-30 19:01:11 +0100

    thisisafix

---
+++ app/src/main/java/com/deltadna/notification_test/MyGcmListenerService.java
@@ -0,0 +1,7 @@
+package com.deltadna.notification_test;
+
+/**
+ * Created by steven on 29/09/15.
+ */
+public class MyGcmListenerService {
+}
---
+++ app/src/main/java/com/deltadna/notification_test/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.deltadna.notification_test;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by steven on 29/09/15.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService{
+}
